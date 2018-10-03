56a9e9f1224625bd9dc1dd44625a8381e33e049f
Shekhar Karande <shehar.karande@gmail.com>
2018-10-03 09:57:14 +0100

    adding string lessons

---
+++ learn_enough_ruby/strings_lesson/ex_2_5_1.rb
@@ -0,0 +1,11 @@
+test_string = "hoNey BaDGer"
+
+check_string = "badger"
+
+puts test_string.downcase.include? check_string
+
+puts check_string.upcase!
+puts check_string.upcase!
+
+puts check_string.nil?
+puts "".nil?
